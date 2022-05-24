4b924e09a6d4e4e4153096caba7578fda9d84ed3
zzrjim <zzrjim2010@gmail.com>
2022-05-24 09:33:36 +0800

    123

--- src/js/riding.js
+++ src/js/riding.js
@@ -2,3 +2,19 @@ require("../css/riding.less");
 let utils = require("./utils.js");
 utils.getfooter("drill");
 utils.getheader("riding");
+let go = document.querySelector("#go");
+let timer = null;
+let num = 4;
+go.onclick = function () {
+  timer = setInterval(function () {
+    console.log(111);
+    num--;
+    go.innerHTML = num;
+    if (num <= 0) {
+      go.innerHTML = "GO";
+      location.href = "../Inmovement.html";
+      clearInterval(timer);
+    }
+  }, 1000);
+};
+// location.href = './详情页01.html?uid='+this.dataset['uid']+'&title='+this.dataset['title'];
