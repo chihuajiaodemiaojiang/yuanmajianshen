5551c0027ef1b522e93f31f0e4600d56c63046f5
zzrjim <1641563543@qq.com>
2022-05-27 22:41:06 +0800

    5.24

--- src/js/commoncss.js
+++ src/js/commoncss.js
@@ -2,3 +2,4 @@ require("../css/commcss/normalize.css");
 require("../css/commcss/reset.css");
 require("../css/commcss/basic.less");
 require("../css/commcss/property.css");
+require("../css/commcss/iconfont.css");
